b9e6153c1a45eeabf07049a8613a10dba4773fd1
Justin Port <justinkredible94@gmail.com>
2026-01-29 16:06:05 -0500

    replace Vite demo with basic portfolio hero section

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+  <>
+    <h1>Justin Port</h1>
+    <h2>Junior Front-End Developer</h2>
+    <p>
+      I’m a front-end developer focused on building clean, responsive web
+      interfaces with React.
+    </p>
+  </>
+)
+}
+
+export default App
